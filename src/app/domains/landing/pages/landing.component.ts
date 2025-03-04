cd8c7da8589f91bab6d766570f34eb88a19d7ff8
Danniel Navas <dannielnavas@gmail.com>
2025-03-03 20:58:10 -0500

    🎉 Agregar componente de pie de página y estilos; incluir en la página de aterrizaje

--- src/app/domains/landing/pages/landing.component.ts
+++ src/app/domains/landing/pages/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../components/banner/banner.component';
+import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
